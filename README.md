810ba7afbceebf5ff16ff700e8a0e3b9e438bba4
Nishit Shah <ngshah3@myseneca.ca>
2022-08-11 01:22:03 -0400

    eorros in push

--- README.md
+++
@@ -1,4 +0,0 @@
-# final_web222
-Final Project for WEB222
-
-Link for this:  https://NishitShah18.github.io/final_web222/
--- js/form.js
+++
@@ -1,9 +0,0 @@
-function hireWage() {
-  var wage = document.getElementById("hourly_wage");
-
-  if (document.getElementById("hiring").checked) {
-    wage.style.display = "inline";
-  } else {
-    wage.style.display = "none";
-  }
-}
